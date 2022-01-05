3a1ebd54be91293fcc0e477ead8d03c80bcfb552
jcerna03 <46335419+jcerna03@users.noreply.github.com>
2022-01-05 15:50:46 -0500

    Add files via upload
    Javascript Solution

---
+++ solution.js
@@ -0,0 +1,30 @@
+const words = ["kellogg", "go", "hola", "lego", "hug", "kocomo", "hello"];
+const letters = ["a", "h", "l", "e", "l", "o", "g", "k"];
+const filterwords = [];
+let letters_map = letters.map(x => x) 
+let validator = 0;
+let longestCount = 0;
+
+for (let word of words) {
+  validator = 0 
+  letters_map = letters.map(x => x) 
+  for (let word_letter of word.split("")) {
+    for (let [i, letter] of letters_map.entries()) {
+      if (word_letter === letter) {
+        let idx = letters_map.indexOf(letter)
+        letters_map.splice(idx,1)
+        validator++;
+        break;
+      }
+    }
+    if (validator === word.length) {
+      filterwords.push(word);
+    }
+  }
+}
+for (const word of filterwords) {
+  if (word.length > longestCount) {
+    longestCount = word.length;
+  }
+}
+console.log(longestCount)
